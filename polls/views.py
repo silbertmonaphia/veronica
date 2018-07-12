dc466049c53bafd7fd1c9252c33d25326d4f92ad
smona <silbertmonaphia@gmail.com>
2018-07-12 23:19:28 +0800

    Refine

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 # encoding:utf-8
 
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+import json
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 
 
 def index(request):
@@ -11,12 +13,23 @@ def index(request):
     output = ', '.join([q.question_text for q in lastest_question_list])
     return HttpResponse(output)
 
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
+
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return json.loads({'error': "You did't select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return json.loads({'question_id': question.id})
