4cedba1bccf19ba49aecbb8f49d18bc12d1625bd
smona <silbertmonaphia@gmail.com>
2019-12-13 15:20:34 +0800

    feat: Redo project UserCenter to veronica

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -13,6 +14,7 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -20,4 +22,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
--- polls/views.py
+++ polls/views.py
@@ -1,35 +1,6 @@
-#! /usr/bin/env python
-# encoding:utf-8
-
-import json
-
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Choice
+from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in lastest_question_list])
-    return HttpResponse(output)
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return json.loads({'error': "You did't select a choice"})
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return json.loads({'question_id': question.id})
+    return HttpResponse("Hello, world. You're at the polls index.")
