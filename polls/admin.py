d4f66832256ee1a31b1f80def3f0c3f6b92519da
smona <silbertmonaphia@gmail.com>
2019-12-18 17:05:51 +0800

    feat: add admin templates

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Date information', {'fields': ['pub_date']}),
+        ('Other information',               {'fields': ['question_text']}),   
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text', 'pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
