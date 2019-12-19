1741e561a4441a4365480d60bddae402b254ce44
smona <silbertmonaphia@gmail.com>
2019-12-19 11:18:29 +0800

    chore: change entrance url:api/v1/

--- veronica/urls.py
+++ veronica/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('polls/', include('apps.api.urls')),
+    path('api/v1/', include('apps.api.urls')),
 ]
